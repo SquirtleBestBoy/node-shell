e4ff90a477e274a6e3db0f54e9b211ceff870721
EricDaoNguyen <EricDaoNguyen@Gmail.com>
2021-08-06 15:41:30 -0400

    added ls.js and pwd.js

--- bash.js
+++ bash.js
@@ -1,8 +1,13 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
-  if(cmd == 'pwd') process.stdout.write(process.cwd());
-  else process.stdout.write('You typed: ' + cmd);
-  process.stdout.write('\nprompt > ');
+  if(cmd == 'pwd') pwd();
+  if(cmd == 'ls') ls();
+  else {
+    process.stdout.write('You typed: ' + cmd);
+  }
 });
---
+++ ls.js
@@ -0,0 +1,9 @@
+module.exports = function () {
+  const fs = fs.readdir('./', 'utf8', (err, files) => {
+    if(err) throw err;
+    else {
+      process.stdout.write(files.join('\n'));
+      process.stdout.write('prompt > ');
+    }
+  })
+};
