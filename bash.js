2f2a8ca1657165c3c7e3ea367478728fde2c86ae
SquirtleBestBoy <wleet@oberlin.edu>
2021-08-06 19:51:46 +0000

    Main work finished

--- bash.js
+++ bash.js
@@ -6,8 +6,8 @@ process.stdout.write('prompt > ');
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
   if(cmd == 'pwd') pwd();
-  if(cmd == 'ls') ls();
+  else if(cmd == 'ls') ls();
   else {
-    process.stdout.write('You typed: ' + cmd);
+    process.stdout.write('You typed: ' + cmd + '\nprompt >');
   }
 });
--- ls.js
+++ ls.js
@@ -1,9 +1,10 @@
+const fs = require('fs');
 module.exports = function () {
-  const fs = fs.readdir('./', 'utf8', (err, files) => {
+  fs.readdir('./', 'utf8', (err, files) => {
     if(err) throw err;
     else {
       process.stdout.write(files.join('\n'));
-      process.stdout.write('prompt > ');
+      process.stdout.write('\nprompt > ');
     }
   })
 };
