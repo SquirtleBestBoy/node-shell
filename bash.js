2c37e0621f088c18e16817e3529c6f799598efaf
SquirtleBestBoy <wleet@oberlin.edu>
2021-08-06 20:01:19 +0000

    cat function complete

--- bash.js
+++ bash.js
@@ -1,12 +1,15 @@
 const pwd = require('./pwd');
 const ls = require('./ls');
+const cat = require('./cat');
 
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
+  const args = cmd.split(' ');
   if(cmd == 'pwd') pwd();
   else if(cmd == 'ls') ls();
+  else if (args[0] == 'cat') cat(args[1])
   else {
     process.stdout.write('You typed: ' + cmd + '\nprompt >');
   }
